2f44b02845ca8290d13443099322d6804443edfb
Slan <snchong.slan@gmail.com>
2022-06-23 05:14:54 +0800

    first commit

---
+++ components/Pokemon.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Image from "next/image";
+
+const Pokemon = ({ pokemon, index }) => {
+  const [hover, setHover] = useState(false);
+  const pokeIndex = ("000" + (index + 1)).slice(-3);
+  const src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokeIndex}.png`;
+  return (
+    <div
+      className="bg-gray-800 hover:bg-gray-600 rounded-xl flex flex-col justify-center items-center relative "
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Image
+        loader={() => src}
+        unoptimized={true}
+        src={src}
+        width={150}
+        height={150}
+        alt={pokemon.name}
+      ></Image>
+      <div
+        className={`opacity-50 absolute text-3xl text-white top-0 right-3 font-semibold ${
+          hover && "opacity-100"
+        }`}
+      >
+        #{pokeIndex}
+      </div>
+      <div className={`text-gold-400 m-2 ${hover && "text-gold-300"}`}>
+        {pokemon.name}
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          200: "rgb(255, 210, 84)",
+          300: "rgb(252, 196, 40)",
+          400: "rgb(224, 171, 24)",
+          500: "rgb(197, 132, 11)",
+          600: "rgb(158, 114, 18)",
+          700: "rgb(143, 90, 21)",
+        },
+        gray: {
+          900: "rgb(28, 28, 31)",
+          800: "rgb(42, 42, 45)",
+          700: "#383838",
+          600: "rgb(65, 65, 65)",
+          500: "rgb(78, 78, 78)",
+          400: "rgb(94, 94, 94)",
+          300: "rgb(124, 124, 124)",
+          200: "#acacac",
+          150: "#dcdcdc",
+        },
+      },
+      opacity: {
+        15: ".15",
+      },
+      transitionProperty: {
+        height: "height",
+        spacing: "margin, padding",
+        rounded: "border-radius, background-color",
+      },
+      height: {
+        "main-electron": "calc(100vh - 45px)",
+        "main-web": "100vh",
+      },
+      padding: {
+        nav: "43px",
+      },
+      dropShadow: {
+        dark: "0 5px 2px rgba(0, 0, 0, 0.8)",
+      },
+      boxShadow: {
+        glow: "0 0 12px 0px",
+        "glow-sm": "0 0 4px 0px",
+      },
+    },
+  },
+  plugins: [],
+};
